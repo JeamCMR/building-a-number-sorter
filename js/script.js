6c6b78ca56434715d1979145975895670b635526
COPSTA <tutomx16@gmail.com>
2024-04-11 16:47:11 -0500

    Añade algoritmo de ordemaniento por seleccion

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ const sortInputArray = (event) =>{
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown)=>Number(dropdown.value));
     // console.log(inputValues);
 
-    const sortedValues  = bubbleSort (inputValues);
+    // const sortedValues  = bubbleSort (inputValues);
+    const sortedValues   = selectionSort (inputValues);
     // console.log(sortedValues);
     updateUI(sortedValues);
 }
@@ -24,23 +25,43 @@ const updateUI  = (array = [])=>{ //Se pasa un parametro con un valor alternativ
 }
 
 
-//Algoritmos Burbuja
+//Algoritmos de clasificacion de Burbuja
 
 const bubbleSort  =(array) =>{
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length-1; j++) {
-        console.log(array, array[j] ,array[j+1])
+        // console.log(array, array[j] ,array[j+1])
         if (array[j] > array[j + 1]) {
             let temp = array[j];
             array[j] = array[j + 1];
             array[j + 1] = temp;
         }
-        
     }
-    
   }
   return array;
 }
+
+
+//Algoritmo de clasificacion por seleccion [Busca el elemento mas pequño y lo colaca en la primera posicion]
+
+const selectionSort  = (array) =>{
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++){
+        // console.log(array, array[j], array[minIndex])
+        if(array[j] < array[minIndex]){
+          minIndex = j;
+        }
+    }
+    const temp = array[i]; // Guardo el valor
+    array[i] = array[minIndex]; //le paso el valor del Minimo al array[i];
+    array[minIndex] = temp; 
+  }
+  return array;
+}
+
+
+
 
 
 /** EVENTOS **/
