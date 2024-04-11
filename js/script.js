f8c13d7665148a7f7cf22ec0150816505e03b096
COPSTA <tutomx16@gmail.com>
2024-04-11 14:56:36 -0500

    Añade variables y funciones para interactuar con el DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 /**Variables base */
+
+const sortButton = document.getElementById("sort");
+
+
+/**FUNCIONES */
+
+const sortInputArray = (event) =>{
+    event.preventDefault();
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown)=>Number(dropdown.value));
+    console.log(inputValues);
+}
+
+
+/** EVENTOS **/
+
+sortButton.addEventListener("click", sortInputArray)    
